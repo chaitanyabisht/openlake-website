493da60e5f9f717c807a45f91b0fdaece0af5914
KShivendu <shivendu@iitbhilai.ac.in>
2020-05-06 20:18:54 +0530

    Add raw design

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,6 +15,7 @@ import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Header from "./header";
 import "./layout.css";
 import { blue } from "@material-ui/core/colors";
+import { Toolbar, AppBar, Link, Button } from "@material-ui/core";
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -28,6 +29,8 @@ const Layout = ({ children }) => {
   `);
 
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const preventDefault = event => event.preventDefault();
 
   const theme = React.useMemo(
     () =>
@@ -44,14 +47,58 @@ const Layout = ({ children }) => {
     <ThemeProvider theme={theme}>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
+        className="hero"
+        style={{
+          backgroundColor: "#4c4c4c",
+          color: "white",
+          display: "flex",
+          height: "500px",
+          boxSizing: "border-box",
+          padding: "50px",
+        }}
+      >
+        <h1>Welcome to Open Lake</h1>
+      </div>
+
+      <div
         style={{
           margin: `0 auto`,
           maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
+          textAlign: "center",
         }}
       >
         <main>{children}</main>
       </div>
+
+      <div
+        className="explore-projects"
+        style={{
+          textAlign: "center",
+          backgroundColor: "#f2f2f2",
+          padding: "3rem",
+          color: "#444",
+        }}
+      >
+        <h1>2,000 Projects and counting</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium
+          rem id labore inventore, deserunt autem, ab expedita odio delectus
+          ullam recusandae in eveniet eius at architecto vero esse! Modi,
+          quisquam.
+        </p>
+      </div>
+
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <h3>Open Lake</h3>
+          <div style={{ textAlign: "right" }}>
+            <Button edge="end" href="#">Button</Button>
+            <Button href="#">Button</Button>
+            <Button href="#">Button</Button>
+          </div>
+        </Toolbar>
+      </AppBar>
     </ThemeProvider>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,27 @@ import { Link } from "gatsby";
 import Layout from "../components/layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
+import { grey } from "@material-ui/core/colors";
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <h1>Everyone Benefits</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur, ex unde veniam fugiat deleniti iusto, perferendis voluptatibus laborum optio totam ad? Minima architecto reprehenderit rerum autem fugiat unde eum dicta!</p>
+    <div className="images" style={{display: "flex", flexFlow: "row nowrap"}}>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur veritatis quos omnis, soluta reiciendis aliquid, in nostrum asperiores sint ad nesciunt tempora cumque itaque sed reprehenderit libero id, repellendus laborum?</p>
+
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur veritatis quos omnis, soluta reiciendis aliquid, in nostrum asperiores sint ad nesciunt tempora cumque itaque sed reprehenderit libero id, repellendus laborum?</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur veritatis quos omnis, soluta reiciendis aliquid, in nostrum asperiores sint ad nesciunt tempora cumque itaque sed reprehenderit libero id, repellendus laborum?</p>
+    
+    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
+    </div> */}
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+
+
   </Layout>
 );
 
