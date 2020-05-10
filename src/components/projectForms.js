b671057122c46de10b722b58ab81a61bd76e8cde
Meghana <meghana@iitbhilai.ac.in>
2020-05-11 03:34:07 +0530

    added form and ui changes

--- src/components/projectForms.js
+++ src/components/projectForms.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import {
     TextField, 
     Box, 
@@ -8,74 +13,108 @@ import {
     FormHelperText,
     FormControl,
     Select,
-    ExpansionPanel,
-    ExpansionPanelSummary,
-    ExpansionPanelDetails,
-    Typography,
-    ExpandMoreIcon,
+    Grid,
+    Button,
  } from '@material-ui/core';
 
-const useStyles = makeStyles({
-    root: {
-        // width: '100%',
-      },
-    heading: {
-        // fontSize: theme.typography.pxToRem(15),
-        // fontWeight: theme.typography.fontWeightRegular,
-      },
-    formControl: {
-        // margin: theme.spacing(1),
-        minWidth: 120,
-    },
-    selectEmpty: {
-        // marginTop: theme.spacing(2),
-    },
-});
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+  item: {
 
-function ProjectForm() {
+  }
+}));
+
+export default function SimpleExpansionPanel() {
   const formClasses = useStyles();
   const [role, setRole] = React.useState('');
-
   const handleChange = (event) => {
     setRole(event.target.value);
   };
-
   return (
-        <div className={formClasses.root}>
-            <ExpansionPanel>
-                <ExpansionPanelSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                >
-                    <Typography className={formClasses.heading}>Are you working on Project? Let us know more: </Typography>
-                </ExpansionPanelSummary>
-                
-                <ExpansionPanelDetails>
-                    <div>
-                        <TextField id="standard-basic" label="Your Name" /> <br/>
-                        <TextField id="standard-basic" label="Project Name" /> <br/>
-                            <FormControl variant="outlined" className={formClasses.formControl}>
-                                <InputLabel id="demo-simple-select-outlined-label">Role</InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-outlined-label"
-                                    id="demo-simple-select-outlined"
-                                    value={role}
-                                    onChange={handleChange}
-                                    label="Role"
-                                >
-                                <MenuItem value="Contributor">Contributor</MenuItem>
-                                <MenuItem value="Founder">Founder</MenuItem>
-                                </Select>
-                                <FormHelperText>Required</FormHelperText>
-                            </FormControl>
-                        <TextField id="standard-basic" label="Link to the Project" /> <br/>
-                        <TextField id="standard-basic" label="Tech Stack" /> <br/>
-                    </div> 
-                </ExpansionPanelDetails>
-            </ExpansionPanel>
-        </div>
-    );
+    <div className={formClasses.root}>
+      <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography className={formClasses.heading}>Are you working on a project too? We'd love to know more!</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+            <Grid
+            container
+            direction="row"
+            justify="space-evenly"
+            alignItems="center"
+            className={formClasses.title}
+            >
+                <Grid item className={formClasses.item}>
+                <FormControl >
+                <TextField id="standard-basic" label="Your Name" /> <br/>
+                <FormHelperText>Required</FormHelperText>
+                </FormControl>
+                </Grid>
+                <Grid item className={formClasses.item}>
+                <FormControl >
+                <TextField id="standard-basic" label="Contact Number" /> <br/>
+                <FormHelperText>Required</FormHelperText>
+                </FormControl>
+                </Grid>
+                <Grid item>
+                <FormControl >
+                <TextField id="standard-basic" label="Project Name" /> <br/>
+                <FormHelperText>Required</FormHelperText>
+                </FormControl>
+                </Grid>
+                <Grid item>
+                <FormControl >
+                <TextField id="standard-basic" label="Tell us about project" /> <br/>
+                <FormHelperText>Required</FormHelperText>
+                </FormControl>
+                </Grid>
+                <Grid item>
+                <FormControl variant="outlined" className={formClasses.formControl}>
+                    <InputLabel id="demo-simple-select-outlined-label">Role</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-outlined-label"
+                            id="demo-simple-select-outlined"
+                            value={role}
+                            onChange={handleChange}
+                            label="Role"
+                            >
+                        <MenuItem value="Contributor">Contributor</MenuItem>
+                        <MenuItem value="Founder">Founder</MenuItem>
+                        </Select>
+                    <FormHelperText>Required</FormHelperText>
+                </FormControl>
+                </Grid>
+                <Grid item>
+                <FormControl >
+                <TextField id="standard-basic" label="Link to the Project" /> <br/>
+                <FormHelperText>Required</FormHelperText>
+                </FormControl>
+                </Grid>
+                <Grid item>
+                <FormControl >
+                <TextField id="standard-basic" label="Tech Stack" /> <br/>
+                <FormHelperText>Required</FormHelperText>
+                </FormControl>
+                </Grid>
+                <Grid>
+                <Button variant="contained" color="primary">
+                    Submit
+                </Button>
+                </Grid> 
+            </Grid>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+  
+    </div>
+  );
 }
-
-export default ProjectForm 
