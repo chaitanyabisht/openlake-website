05582beaeb15d3b5206c97fd32af2538ff13a13a
KShivendu <shivendu@iitbhilai.ac.in>
2020-05-10 23:30:48 +0530

    Minor changes in Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "gatsby";
-
 import Layout from "../components/layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
 import { Container, Typography, Paper } from "@material-ui/core";
 
-const IndexPage = () => (
-  <Layout>
+
+export default function IndexPage() {
+
+  return (
+    <Layout>
     <SEO title="Home" />
     <div
       style={{
@@ -16,42 +18,45 @@ const IndexPage = () => (
         padding: "50px",
       }}
     >
-      <Typography variant="h2">Welcome to Open Lake</Typography>
+      <Typography variant="h1">Open Lake</Typography>
+
+      <Typography paragraph>
+        Connecting Students to the World of Open Source Development.
+      </Typography>
     </div>
 
     <Paper elevation="0">
       <Container maxWidth="lg" style={{ padding: "3rem" }}>
-        <Typography variant="h3" gutterBottom>
-          Everyone Benefits
+        <Typography variant="h4" gutterBottom>
+          What is OpenLake
+        </Typography>
+
+        <Typography paragraph >
+          As the world is moving towards a connected society. The communities
+          driving the development of open source technologies are becoming more
+          prominent. Numerous software giants like Google and Microsoft are
+          embracing the idea of Open Source Software. With Microsoft's Campaign:
+          “ Microsoft ❤️ Open Source ”, the importance of open source is visible
+          to the world.
         </Typography>
 
         <Typography paragraph>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur, ex
-          unde veniam fugiat deleniti iusto, perferendis voluptatibus laborum
-          optio totam ad? Minima architecto reprehenderit rerum autem fugiat
-          unde eum dicta!
+          For example, Tensorflow and Pytorch are 2 very important Machine
+          learning libraries that heavily govern the advancements in Artificial
+          Intelligence today, both of these are driven and developed by open
+          source communities in collaboration with corporate firms.
+        </Typography>
+
+        <Typography paragraph>
+        With the importance of Open Source made crystal clear. There is one problem that needs to be addressed: Connecting people with Open Source Communities. The idea of OpenLake is to act as a bridge between Students and the World of Open Source Development.
         </Typography>
 
         <div style={{ display: "flex", flexFlow: "row nowrap" }}>
-          <Typography paragraph>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
-            veritatis quos omnis, soluta reiciendis aliquid, in nostrum
-            asperiores sint ad nesciunt tempora cumque itaque sed reprehenderit
-            libero id, repellendus laborum?
-          </Typography>
-
-          <Typography paragraph>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
-            veritatis quos omnis, soluta reiciendis aliquid, in nostrum
-            asperiores sint ad nesciunt tempora cumque itaque sed reprehenderit
-            libero id, repellendus laborum?
-          </Typography>
-          <Typography paragraph>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
-            veritatis quos omnis, soluta reiciendis aliquid, in nostrum
-            asperiores sint ad nesciunt tempora cumque itaque sed reprehenderit
-            libero id, repellendus laborum?
-          </Typography>
+          {/* <Typography paragraph>
+            
+          </Typography> */}
+          {/* 
+          <Typography paragraph></Typography> */}
         </div>
       </Container>
     </Paper>
@@ -74,6 +79,8 @@ const IndexPage = () => (
       </Typography>
     </div>
   </Layout>
-);
 
-export default IndexPage;
+  )
+}
+
+
