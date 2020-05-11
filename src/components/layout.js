bb3c948bacb97d84c2672b1ec4c6843b12ab6b3c
Ambar Mutha <ambar.ytl@gmail.com>
2020-05-11 14:16:14 +0530

    🎨 Minor UI changes to index page and footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
 
-import { Toolbar, AppBar, Button } from '@material-ui/core';
+import { Button, Paper } from '@material-ui/core';
 
 import Header from './header';
 import './layout.css';
@@ -31,17 +31,24 @@ const Layout = ({ children }) => {
 
       <main>{children}</main>
 
-      <div style={{display: "flex", flexDirection: "row", backgroundColor : "#444" , color : "white"}}>
-        <h3>Open Lake</h3>
-        <div style={{display : "inherit"}}>
+      <Paper
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          padding: '0 24px',
+        }}
+      >
+        <h3>{data.site.siteMetadata.title}</h3>
+        <div style={{ display: 'inherit' }}>
           <Button edge="end" href="#">
             Button
           </Button>
           <Button href="#">Button</Button>
           <Button href="#">Button</Button>
         </div>
-      </div>
-      </>
+      </Paper>
+    </>
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,86 +1,98 @@
-import React from "react";
-import { Link } from "gatsby";
-import Layout from "../components/layout";
-import Image from "../components/image";
-import SEO from "../components/seo";
-import { Container, Typography, Paper } from "@material-ui/core";
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
+import { Container, Typography, Paper, makeStyles } from '@material-ui/core';
 
+const useStyles = makeStyles({
+  centerText: {
+    textAlign: 'center',
+  },
+});
 
 export default function IndexPage() {
+  const classes = useStyles();
 
   return (
     <Layout>
-    <SEO title="Home" />
-    <div
-      style={{
-        height: "500px",
-        boxSizing: "border-box",
-        padding: "50px",
-      }}
-    >
-      <Typography variant="h1">Open Lake</Typography>
-
-      <Typography paragraph>
-        Connecting Students to the World of Open Source Development.
-      </Typography>
-    </div>
-
-    <Paper elevation="0">
-      <Container maxWidth="lg" style={{ padding: "3rem" }}>
-        <Typography variant="h4" gutterBottom>
-          What is OpenLake
-        </Typography>
-
-        <Typography paragraph >
-          As the world is moving towards a connected society. The communities
-          driving the development of open source technologies are becoming more
-          prominent. Numerous software giants like Google and Microsoft are
-          embracing the idea of Open Source Software. With Microsoft's Campaign:
-          “ Microsoft ❤️ Open Source ”, the importance of open source is visible
-          to the world.
-        </Typography>
+      <SEO title="Home" />
+      <div
+        className={classes.centerText}
+        style={{
+          height: '500px',
+          boxSizing: 'border-box',
+          padding: '50px',
+        }}
+      >
+        <Typography variant="h1">Open Lake</Typography>
 
         <Typography paragraph>
-          For example, Tensorflow and Pytorch are 2 very important Machine
-          learning libraries that heavily govern the advancements in Artificial
-          Intelligence today, both of these are driven and developed by open
-          source communities in collaboration with corporate firms.
+          Connecting Students to the World of Open Source Development.
         </Typography>
+      </div>
 
-        <Typography paragraph>
-        With the importance of Open Source made crystal clear. There is one problem that needs to be addressed: Connecting people with Open Source Communities. The idea of OpenLake is to act as a bridge between Students and the World of Open Source Development.
-        </Typography>
+      <Paper className={classes.centerText} elevation={0}>
+        <Container maxWidth="lg" style={{ padding: '3rem' }}>
+          <Typography variant="h4" gutterBottom>
+            What is OpenLake
+          </Typography>
 
-        <div style={{ display: "flex", flexFlow: "row nowrap" }}>
-          {/* <Typography paragraph>
+          <Typography paragraph>
+            As the world is moving towards a connected society. The communities
+            driving the development of open source technologies are becoming
+            more prominent. Numerous software giants like Google and Microsoft
+            are embracing the idea of Open Source Software. With Microsoft's
+            Campaign: “ Microsoft{' '}
+            <span role="img" aria-label="Heart">
+              ❤️
+            </span>{' '}
+            Open Source ”, the importance of open source is visible to the
+            world.
+          </Typography>
+
+          <Typography paragraph>
+            For example, Tensorflow and Pytorch are 2 very important Machine
+            learning libraries that heavily govern the advancements in
+            Artificial Intelligence today, both of these are driven and
+            developed by open source communities in collaboration with corporate
+            firms.
+          </Typography>
+
+          <Typography paragraph>
+            With the importance of Open Source made crystal clear. There is one
+            problem that needs to be addressed: Connecting people with Open
+            Source Communities. The idea of OpenLake is to act as a bridge
+            between Students and the World of Open Source Development.
+          </Typography>
+
+          <div style={{ display: 'flex', flexFlow: 'row nowrap' }}>
+            {/* <Typography paragraph>
             
           </Typography> */}
-          {/* 
+            {/* 
           <Typography paragraph></Typography> */}
-        </div>
-      </Container>
-    </Paper>
+          </div>
+        </Container>
+      </Paper>
 
-    <div
-      className="explore-projects"
-      style={{
-        textAlign: "center",
-        padding: "3rem",
-      }}
-    >
-      <Typography variant="h3" gutterBottom>
-        2,000 Projects and counting
-      </Typography>
-      <Typography paragraph>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
-        veritatis quos omnis, soluta reiciendis aliquid, in nostrum asperiores
-        sint ad nesciunt tempora cumque itaque sed reprehenderit libero id,
-        repellendus laborum?
-      </Typography>
-    </div>
-  </Layout>
-
-  )
+      <div
+        className="explore-projects"
+        style={{
+          textAlign: 'center',
+          padding: '3rem',
+        }}
+      >
+        <Typography variant="h3" gutterBottom>
+          2,000 Projects and counting
+        </Typography>
+        <Typography paragraph>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
+          veritatis quos omnis, soluta reiciendis aliquid, in nostrum asperiores
+          sint ad nesciunt tempora cumque itaque sed reprehenderit libero id,
+          repellendus laborum?
+        </Typography>
+      </div>
+    </Layout>
+  );
 }
-
-
