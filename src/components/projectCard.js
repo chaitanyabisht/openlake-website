5e1659a687fa54d15fed1a5b6f7309f3a0211d80
Meghana Varanasi <44519203+Meghana-12@users.noreply.github.com>
2020-05-15 19:01:16 +0530

    fixes

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -51,7 +51,7 @@ export default function ProjectCard(x) {
   return (
     <Grid container direction="column" item lg={4} key={x.Name}>
       <Card className={cardclasses.root} >
-        <CardActionArea>
+//         <CardActionArea>
           <Grid
             container
             direction="column"
@@ -100,7 +100,7 @@ export default function ProjectCard(x) {
               </Grid>
             </Grid>
           </CardContent>
-        </CardActionArea>
+//         </CardActionArea>
         <CardActions>
           <Grid container direction="row" justify="space-between">
             <Button size="small" color="primary" href={x.Link}>
